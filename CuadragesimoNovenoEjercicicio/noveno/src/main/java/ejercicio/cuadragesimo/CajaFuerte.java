2f92a51e6f7eb72990f17377fb71aa1783de883e
Jmonarca <jmonarca522@gmail.com>
2025-08-04 11:36:55 -0600

    Agregar implementación de la clase CajaFuerte con generación dinámica de clave y validación en la clase Main.

--- CuadragesimoNovenoEjercicicio/noveno/src/main/java/ejercicio/cuadragesimo/CajaFuerte.java
+++ CuadragesimoNovenoEjercicicio/noveno/src/main/java/ejercicio/cuadragesimo/CajaFuerte.java
@@ -1,9 +1,18 @@
 package ejercicio.cuadragesimo;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 
 public class CajaFuerte {
-    private final String CLAVE = "a1b2";
+    private final String CLAVE;
+
+    public CajaFuerte(){
+        CLAVE = generarClave();
+    }
+
+    public String getClave() {
+        return CLAVE;
+    }
 
     public boolean abrir(String clave) {
         return CLAVE.equals(clave);
@@ -17,5 +26,52 @@ public class CajaFuerte {
             }
         }
         return digitosCoinciden;
+    }
+
+    private String generarClave(){
+        StringBuilder clave = new StringBuilder();
+        String[] numeros = {"1","2","3"};
+        String[] letras = {"A","B","C"};
+        ArrayList<String> caracteresRepetidos = new ArrayList<>(); 
+
+        SecureRandom random = new SecureRandom();
+
+        while (clave.length() < 4){
+            if (random.nextBoolean()) {
+                String nuevoCaracter = numeros[random.nextInt(numeros.length)];
+                if(!caracteresRepetidos.contains(nuevoCaracter)) {
+                    clave.append(nuevoCaracter);
+                    caracteresRepetidos.add(nuevoCaracter);
+                }
+            } else {
+                String nuevoCaracter = letras[random.nextInt(letras.length)];
+                if(!caracteresRepetidos.contains(nuevoCaracter)) {
+                    clave.append(nuevoCaracter);
+                    caracteresRepetidos.add(nuevoCaracter);
+                }
+            }
+        }
+        return clave.toString();
+    }
+
+    private boolean coincidenciaDeCaracter(char letra){
+        for (int i = 0; i < CLAVE.length(); i++){
+            if(CLAVE.charAt(i) == letra){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String nivelCoincidencia(char letra , int index){
+        String mensage;
+        if (coincidenciaDeCaracter(letra) && CLAVE.charAt(index) == letra) {
+            mensage = letra+" coincide y está en la posición correcta";
+        } else if (coincidenciaDeCaracter(letra)) {
+            mensage = letra+" coincide pero no está en la posición correcta";
+        } else {
+            mensage = letra+" no coincide";
+        }
+        return mensage;
     }
 }
--- CuadragesimoNovenoEjercicicio/noveno/src/main/java/ejercicio/cuadragesimo/Main.java
+++ CuadragesimoNovenoEjercicicio/noveno/src/main/java/ejercicio/cuadragesimo/Main.java
@@ -9,12 +9,18 @@ public class Main {
             boolean abierto = false;
             int intentos = 0;
 
-            while(!abierto || intentos <=10){
+            while(!abierto && intentos <=10){
                 System.out.print("Introduce la clave de la caja fuerte: ");
                 String clave = scanner.nextLine();
                 intentos++;
 
                 if(clave.length() == 4){
+                    if(clave.matches("[A-C1-3]+")) {
+                        System.out.println("Clave válida.");
+                    } else {
+                        System.out.println("La clave solo puede contener letras A, B, C y números 1, 2, 3.");
+                        continue;
+                    }
                     if (cajaF.abrir(clave)) {
                         abierto = true;
                         System.out.println("¡Caja fuerte abierta!");
@@ -23,6 +29,9 @@ public class Main {
                         System.out.println("Clave incorrecta. Inténtalo de nuevo.");
                         if(!cajaF.obtenerDigitosCoinciden(clave).isEmpty()){
                             System.out.println("Los dígitos que coinciden son: " + cajaF.obtenerDigitosCoinciden(clave));
+                            for (int i = 0; i < clave.length(); i++) {
+                                System.out.println(cajaF.nivelCoincidencia(clave.charAt(i), i));
+                            }
                         }
                     }
                 }else {
